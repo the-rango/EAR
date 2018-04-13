b83959c50d11a990daac12a0e1f6670c7232686b
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-04-12 19:24:17 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+from urllib.parse import urlparse
+import os
+import redis
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def main():
+    val = None
+    if request.method == 'POST':
+        url = urlparse(os.environ.get('REDISCLOUD_URL'))
+        val = eval(redis.Redis(host=url.hostname, port=url.port, password=url.password).get(request.form['key']))
+    return render_template('db.html', val=val)
