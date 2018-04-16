378b64e5e521e8ce9704a729ca14441bc5fd612f
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-04-15 19:58:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,22 @@ import redis
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def main():
+    return 'Hi!'
+
+@app.route('/rc', methods=['GET','POST'])
+def rc():
     val = None
     if request.method == 'POST':
         url = urlparse(os.environ.get('REDISCLOUD_URL'))
         val = redis.Redis(host=url.hostname, port=url.port, password=url.password).get(request.form['key'])
-    return render_template('db.html', val=val)
+    return render_template('rc_db.html', val=val)
+
+@app.route('/rg', methods=['GET','POST'])
+def rg():
+    val = None
+    if request.method == 'POST':
+        url = urlparse(os.environ.get('REDISGREEN_URL'))
+        val = redis.Redis(host=url.hostname, port=url.port, password=url.password).get(request.form['key'])
+    return render_template('rg_db.html', val=val)
