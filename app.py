2b0525ab0676dcd3efe5caac8764863f69eb8e6d
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-04-15 20:04:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 def main():
     return 'Hi!'
 
-@app.route('/rc', methods=['GET','POST'])
-def rc():
+@app.route('/rtg', methods=['GET','POST'])
+def rtg():
     val = None
     if request.method == 'POST':
-        url = urlparse(os.environ.get('REDISCLOUD_URL'))
+        url = urlparse(os.environ.get('REDISTOGO_URL'))
         val = redis.Redis(host=url.hostname, port=url.port, password=url.password).get(request.form['key'])
-    return render_template('rc_db.html', val=val)
+    return render_template('rtg_db.html', val=val)
 
 @app.route('/rg', methods=['GET','POST'])
 def rg():
