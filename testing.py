5151ed9a02e38db0dda95123088852bdd6244469
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-04-17 13:49:35 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -7,7 +7,7 @@ url = urlparse(os.environ.get('REDISTOGO_URL'))
 r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
 
 for gvkey in r.scan_iter():
-    handles, l_id = r.get(gvkey)
+    handles, l_id = eval(r.get(gvkey))
     if len(handles) == 1:
         r.set(gvkey, {handles[0]:l_id})
 
